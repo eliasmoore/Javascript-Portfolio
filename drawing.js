4071d8fbe903f11af0b7990d1a9df06ec5d798af
eliasmoore <emoore@whiteoutsolutions.com>
2018-05-24 13:51:31 -0400

    Finished drawing page

--- drawing.js
+++ drawing.js
@@ -1,6 +1,7 @@
 var c = document.getElementById("board");
 var ctx = c.getContext("2d");
 
+
 var mousePosX;
 var mousePosY;
 
@@ -10,12 +11,12 @@ c.addEventListener("mousemove", function(evt){getMousePos(evt, c);});
 c.addEventListener("mousedown", function(evt){mouseIsDown = true});
 c.addEventListener("mouseup", function(evt){mouseIsDown = false});
 
-setInterval(draw, 1);
+setInterval(draw, .1);
 function draw()
 {
   if(mouseIsDown)
   {
-    ctx.fillRect(mousePosX - 5,mousePosY - 5,10,10);
+    ctx.fillRect(mousePosX - 5,mousePosY - 5,10,10);.
   }
   
 }
